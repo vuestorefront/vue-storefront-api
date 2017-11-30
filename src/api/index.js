245c5220af5838eb8d0a31043dce25013fd23aa5
pkarw <pkarwatka@divante.pl>
2017-11-30 18:04:06 +0100

    Stock items proxy

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ import order from './order';
 import img from './img';
 import catalog from './catalog'
 import user from './user'
+import stock from './stock'
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -16,6 +17,9 @@ export default ({ config, db }) => {
 	
 	// mount the user reosource
 	api.use('/user', user({ config, db }));
+
+	// mount the stock reosource
+	api.use('/stock', stock({ config, db }));
 	
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
---
+++ src/platform/magento2/stock.js
@@ -0,0 +1,17 @@
+import AbstractUserProxy from '../abstract/user'
+
+
+class StockProxy extends AbstractUserProxy {
+    constructor (config){
+        const Magento2Client = require('./magento2-rest-client').Magento2Client;
+        super(config)
+        this.api = Magento2Client(config.magento2.api);
+    }       
+
+    check (sku) { 
+        return this.api.stockItems.list(sku)
+    }
+    
+}
+
+module.exports = StockProxy
