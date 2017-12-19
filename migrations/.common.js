2087567596008bf6481c91d9158fee167e65b21e
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2017-12-19 15:30:57 +0600

    Fixing request timeout error

--- migrations/.common.js
+++ migrations/.common.js
@@ -8,7 +8,8 @@ let client = new es.Client({
   host: config.esHost,
   log: 'debug',
   apiVersion: '5.5',
-  requestTimeout: 10000
+  requestTimeout: 10000,
+  keepAlive: false
 })
 
 exports.db = client        
