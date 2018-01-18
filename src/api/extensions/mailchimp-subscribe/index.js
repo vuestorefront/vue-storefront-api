5b87abd278640434c65441c71bf09b563e289662
pkarw <pkarwatka@divante.pl>
2018-01-18 21:21:47 +0100

    Fix for https://github.com/DivanteLtd/vue-storefront/issues/523

--- src/api/extensions/mailchimp-subscribe/index.js
+++ src/api/extensions/mailchimp-subscribe/index.js
@@ -12,7 +12,6 @@ module.exports = ({ config, db }) => {
 	mcApi.post('/subscribe', (req, res) => {
 
 		let userData = req.body
-console.log(userData)
 		if(!userData.email) {
 			apiStatus(res, 'Invalid e-mail provided!', 500)
 			return
@@ -24,7 +23,7 @@ console.log(userData)
 			method: 'POST',
 			headers: { 'Authorization': 'apikey ' + config.extensions.mailchimp.apiKey },
 			json: true,
-			body: { email_address: userData.email, status: 'pending' }
+			body: { email_address: userData.email, status: 'subscribed' }
 		}, function (error, response, body) {
 			if (error) {
 				apiStatus(res, error, 500)
@@ -33,5 +32,32 @@ console.log(userData)
 			}
 		})
 	})
+
+	/** 
+	 * DELETE delete an user
+	 */
+	mcApi.delete('/subscribe', (req, res) => {
+
+		let userData = req.body
+		if(!userData.email) {
+			apiStatus(res, 'Invalid e-mail provided!', 500)
+			return
+		}
+		
+		let request = require('request');
+		request({
+			url: config.extensions.mailchimp.apiUrl + '/lists/' + config.extensions.mailchimp.listId,
+			method: 'POST',
+			headers: { 'Authorization': 'apikey ' + config.extensions.mailchimp.apiKey },
+			json: true,
+			body: { members: [ { email_address: userData.email, status: 'unsubscribed' } ], "update_existing": true }
+		}, function (error, response, body) {
+			if (error) {
+				apiStatus(res, error, 500)
+			} else {
+				apiStatus(res, body, 200)
+			}
+		})
+	})	
 	return mcApi
 }
