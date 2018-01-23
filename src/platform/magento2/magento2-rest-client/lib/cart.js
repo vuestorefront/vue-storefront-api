9127b577245bf1b6ad02a9fb34327d440750ecbc
pkarw <pkarwatka@divante.pl>
2018-01-23 17:14:14 +0100

    order_2_magento refactor; Closes: https://github.com/DivanteLtd/vue-storefront/issues/504

--- src/platform/magento2/magento2-rest-client/lib/cart.js
+++ src/platform/magento2/magento2-rest-client/lib/cart.js
@@ -1,38 +1,101 @@
 module.exports = function (restClient) {
     var module = {};
     
-    module.create = function (customerToken) {
-        if (customerToken) {
-            return restClient.post('/carts/mine', {}, customerToken);
-        } else 
-        {
-            return restClient.post('/guest-carts');
+    module.create = function (customerToken, customerId = null) {
+        if (customerId) {
+            return restClient.post('/customers/' + customerId + '/carts', {}, customerToken);
+        } else {
+            if (customerToken) {
+                return restClient.post('/carts/mine', {}, customerToken);
+            } else 
+            {
+                return restClient.post('/guest-carts');
+            }
         }
     }    
-    module.update = function (customerToken, cartId, cartItem) {
-        if (customerToken) {
-            return restClient.post('/carts/mine/items', { cartItem: cartItem }, customerToken);
-        } else 
-        {
-            console.log(cartId)
-            return restClient.post('/guest-carts/' + cartId + '/items', { cartItem: cartItem });
+    module.update = function (customerToken, cartId, cartItem, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.post('/carts/' + cartId + '/items/', { cartItem: cartItem });
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.post('/carts/mine/items', { cartItem: cartItem }, customerToken);
+            } else 
+            {
+                return restClient.post('/guest-carts/' + cartId + '/items', { cartItem: cartItem });
+            }
         }
     }    
-    module.delete = function (customerToken, cartId, cartItem) {
-        if (customerToken) {
-            return restClient.delete('/carts/mine/items/' + cartItem.item_id, customerToken);
-        } else 
-        {
-            return restClient.delete('/guest-carts/' + cartId + '/items/' + cartItem.item_id);
+    module.delete = function (customerToken, cartId, cartItem, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.delete('/carts/' + cartId + '/items/' + cartItem.item_id);
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.delete('/carts/mine/items/' + cartItem.item_id, customerToken);
+            } else 
+            {
+                return restClient.delete('/guest-carts/' + cartId + '/items/' + cartItem.item_id);
+            }
         }
     }     
-    module.pull = function (customerToken, cartId, params) {
-        if (customerToken) {
-            return restClient.get('/carts/mine/items', customerToken);
-        } else 
-        {
-            return restClient.get('/guest-carts/' + cartId + '/items/');
+    module.pull = function (customerToken, cartId, params, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.get('/carts/' + cartId + '/items/');
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.get('/carts/mine/items', customerToken);
+            } else 
+            {
+                return restClient.get('/guest-carts/' + cartId + '/items/');
+            }
         }
-    }                
+    }              
+    
+    module.billingAddress = function (customerToken, cartId, body, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.post('/carts/' + cartId + '/billing-address', body);
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.post('/carts/mine/billing-address', body, customerToken);
+            } else 
+            {
+                return restClient.post('/guest-carts/' + cartId + '/billing-address', body);
+            }
+        }
+    }      
+    
+    module.shippingInformation = function (customerToken, cartId, body, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.post('/carts/' + cartId + '/shipping-information', body);
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.post('/carts/mine/shipping-information', body, customerToken);
+            } else 
+            {
+                return restClient.post('/guest-carts/' + cartId + '/shipping-information', body);
+            }
+        }
+    }      
+    
+    module.order = function (customerToken, cartId, body, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.put('/carts/' + cartId + '/order', body);
+        } else {
+            if (customerToken && parseInt(cartId) > 0) {
+                return restClient.put('/carts/mine/order', body, customerToken);
+            } else 
+            {
+                return restClient.put('/guest-carts/' + cartId + '/order', body);
+            }
+        }
+    }       
+
+    module.assign = function (cartId, userId, storeId = 0) {
+        return restClient.put('/guest-carts/' + cartId, 
+            {
+                customerId: userId,
+                storeId: storeId
+            }
+        )
+    }          
     return module;
 }
