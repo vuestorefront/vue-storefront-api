5202f20d822d1425b2157d6e0ff04608f0d325ea
Thomas Thune Hansen <th@improving.dk>
2018-02-20 14:04:18 +0100

    added postdk

---
+++ src/api/extensions/gls-parcelshop-dk/cli.js
@@ -0,0 +1,83 @@
+const config = require('config')
+
+let numCPUs = require('os').cpus().length;
+
+const CommandRouter = require('command-router');
+const cli = CommandRouter();
+
+
+cli.option({
+    name: 'partitions',
+    default: numCPUs,
+    type: Number
+});
+
+cli.command('buildcache', () => {
+    const soap = require('soap')
+    const elasticsearch = require('elasticsearch');
+    const esClient = new elasticsearch.Client({
+        host: config.esHost,
+        log: 'error'
+    });
+
+
+    esClient.indices.exists({index: 'gls_parcelshop_dk'}, (err, resp, status) => {
+        if (!resp) {
+            esClient.indices.create({
+                index: 'gls_parcelshop_dk'
+            }, function (err, resp, status) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("create", resp);
+                }
+            });
+        }
+        else {
+            console.log('Index gls_parcelshop_dk, already exists')
+        }
+    })
+
+    let url = 'http://www.gls.dk/webservices_v2/wsPakkeshop.asmx?wsdl'
+    let bulk = []
+
+    soap.createClient(url, function (err, client) {
+        client.GetAllParcelShops({}, function (err, result) {
+            if (result.GetAllParcelShopsResult) {
+
+
+                for (let i = 0; i < result.GetAllParcelShopsResult.PakkeshopData.length; i++) {
+                    let droppoint = result.GetAllParcelShopsResult.PakkeshopData[i]
+
+                    bulk.push(
+                        {
+                            index: {
+                                _index: 'gls_parcelshop_dk',
+                                _type: 'droppoint',
+                                _id: droppoint.Number
+                            }
+                        }
+                    )
+                    bulk.push(droppoint)
+                }
+            }
+
+            esClient.deleteByQuery({
+                index: 'gls_parcelshop_dk',
+                type: 'droppoint'
+            }, function (err, resp, status) {
+                console.log(resp);
+
+                esClient.bulk({
+                    body: bulk
+                }, function (err, resp, status) {
+                    console.log(resp);
+                })
+            })
+        })
+    })
+});
+
+
+cli.parse(process.argv);
