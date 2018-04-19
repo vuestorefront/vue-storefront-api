361182b3630fbe3fa712aef4e7269a606d032354
pkarw <pkarwatka@divante.pl>
2018-04-19 09:42:32 +0200

    Added support for custom fields into the elastic search index

--- src/lib/elastic.js
+++ src/lib/elastic.js
@@ -1,3 +1,8 @@
+const path = require('path')
+const _ = require('lodash')
+const fs = require('fs');
+const jsonFile = require('jsonfile')
+
 function putAlias(db, originalName, aliasName, next) {
     let step2 = () => { 
         db.indices.putAlias({ index: originalName, name: aliasName }).then(result=>{
@@ -94,113 +99,43 @@ function createIndex(db, indexName, next) {
     })      
 }
 
+/**
+ * Load the schema definition for particular entity type
+ * @param {String} entityType 
+ */
+function loadSchema(entityType) {
+    let elasticSchema = jsonFile.readFileSync(path.join(__dirname, '../../config/elastic.schema.' + entityType + '.json'));
+    const extensionsPath = path.join(__dirname, '../../config/elastic.schema.' + entityType + '.extension.json');
+    if (fs.existsSync(extensionsPath)) {
+        let elasticSchemaExtensions = jsonFile.readFileSync(extensionsPath);
+        elasticSchema = _.merge(elasticSchema, elasticSchemaExtensions) // user extensions
+    }
+    return elasticSchema
+}
+
 function putMappings(db, indexName, next) {
+    let productSchema = loadSchema('product');
+    let taxruleSchema = loadSchema('taxrule');
+    let attributeSchema = loadSchema('attribute')
+
     db.indices.putMapping({
         index: indexName,
         type: "product",
-        body: {
-            properties: {
-                sku: { type: "keyword" },
-                size: { type: "integer" },
-                size_options: { type: "integer" },
-                price: { type: "float" },
-                has_options: { type: "boolean" },            
-                special_price: { type: "float" },
-                color: { type: "integer" },
-                color_options: { type: "integer" },
-                pattern: { type: "text" },
-                id: { type: "long" },
-                status: { type: "integer" },
-                weight: { type: "integer" },
-                visibility: { type: "integer" },
-                created_at: { 
-                    type: "date",           
-                    format: "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                },
-                updated_at: { 
-                    type: "date",           
-                    format: "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                },
-                special_from_date: {
-                    type: "date",           
-                    format: "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                },
-                special_to_date: {
-                    type: "date",           
-                    format: "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                },
-                news_from_date: {
-                    type: "date",           
-                    format: "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                },
-                description: { type: "text" },
-                name: { type: "text" },
-                configurable_children: {
-                    properties: {
-                        has_options: { type: "boolean" },
-                        price: { type: "float" },
-                        sku: { type: "keyword" }
-                    }
-                },
-                configurable_options: {
-                    properties: {
-                        attribute_id: { type: "long" },
-                        default_label: { type: "text"},
-                        label: { type: "text"},
-                        frontend_label: { type: "text"},   
-                        store_label: { type: "text"},
-                        values: {
-                            properties: {
-                                default_label: { type: "text"},
-                                label: { type: "text"},
-                                frontend_label: { type: "text"},                           
-                                store_label: { type: "text"},
-                                value_index:  { type: "keyword" }                          
-                            }
-                        }
-                    }
-                },
-                category_ids: { type: "long" },
-                eco_collection: { type: "integer" },
-                eco_collection_options: { type: "integer" },
-                erin_recommends: { type: "integer" },
-                tax_class_id: { type: "long" }
-            }
-        }
+        body: productSchema        
         }).then(res1 => {
             console.dir(res1, { depth: null, colors: true })
   
             db.indices.putMapping({
                 index: indexName,
                 type: "taxrule",
-                body: {
-                properties: {
-                    id: { type: "long" },
-                    rates: {
-                    properties: {
-                        rate: { type: "float" }
-                    }
-                    }
-                }
-                }
+                body: taxruleSchema
             }).then(res2 => {
                 console.dir(res2, { depth: null, colors: true })
   
                 db.indices.putMapping({
                     index: indexName,
                     type: "attribute",
-                    body: {
-                    properties: {
-                        id: { type: "long" },
-                        attribute_id: { type: "long" },
-  
-                        options: {
-                        properties: {
-                            value:  { type: "text", "index" : "not_analyzed" }                          
-                        }
-                        }
-                    }
-                    }
+                    body: attributeSchema
                 }).then(res3 => {
                     console.dir(res3, { depth: null, colors: true })
                     next()
