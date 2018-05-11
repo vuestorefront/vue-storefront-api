bc23fe823d1685a7eff3d7e811eb5388dfd4f67f
pkarw <pkarwatka@divante.pl>
2018-05-11 10:18:33 +0200

    The API moved to the separate repo: https://github.com/DivanteLtd/magento2-rest-client Added methods to extend the API

--- src/platform/magento2/stock.js
+++ src/platform/magento2/stock.js
@@ -3,7 +3,7 @@ import AbstractUserProxy from '../abstract/user'
 
 class StockProxy extends AbstractUserProxy {
     constructor (config){
-        const Magento2Client = require('./magento2-rest-client').Magento2Client;
+        const Magento2Client = require('magento2-rest-client').Magento2Client;
         super(config)
         this.api = Magento2Client(config.magento2.api);
     }       
