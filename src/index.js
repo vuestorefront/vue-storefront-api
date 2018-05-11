d9dbba5c0556ebbce6e5173c80a53808eca5efd5
gopkumargopi <gopkumargopi@live.in>
2018-05-10 22:38:59 -0700

    Some Code formatting

--- src/index.js
+++ src/index.js
@@ -19,28 +19,28 @@ app.use('/media', express.static(__dirname + config[config.platform].assetPath))
 
 // 3rd party middleware
 app.use(cors({
-	exposedHeaders: config.corsHeaders,
+    exposedHeaders: config.corsHeaders,
 }));
 
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+    limit : config.bodyLimit
 }));
 
 // connect to db
 initializeDb( db => {
 
-	// internal middleware
-	app.use(middleware({ config, db }));
+    // internal middleware
+    app.use(middleware({ config, db }));
 
-	// api router
-	app.use('/api', api({ config, db }));
-	app.use('/img', img({ config, db }));
+    // api router
+    app.use('/api', api({ config, db }));
+    app.use('/img', img({ config, db }));
 
     const port = process.env.PORT || config.server.port
     const host = process.env.HOST || config.server.host
-	app.server.listen(port, host, () => {
-		console.log(`Vue Storefront API started at http://${host}:${port}`);
-	});
+    app.server.listen(port, host, () => {
+        console.log(`Vue Storefront API started at http://${host}:${port}`);
+    });
 });
 
 export default app;
