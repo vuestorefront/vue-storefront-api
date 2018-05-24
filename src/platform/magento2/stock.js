8804d8dcf55f5ff034d303ed1e48eb66a7471841
pkarw <pkarwatka@divante.pl>
2018-05-24 21:34:07 +0200

    Multistore support

--- src/platform/magento2/stock.js
+++ src/platform/magento2/stock.js
@@ -1,11 +1,11 @@
 import AbstractUserProxy from '../abstract/user'
-
+import { multiStoreConfig } from './util'
 
 class StockProxy extends AbstractUserProxy {
     constructor (config, req){
         const Magento2Client = require('magento2-rest-client').Magento2Client;
         super(config, req)
-        this.api = Magento2Client(config.magento2.api);
+        this.api = Magento2Client(multiStoreConfig(config.magento2.api, req));
     }       
 
     check (sku) { 
