c514c5a589b9e13c89394d893058206362066e80
pkarw <pkarwatka@divante.pl>
2018-06-02 17:21:10 +0200

    Tax classes calc fix + new data dumps

--- src/platform/magento2/tax.js
+++ src/platform/magento2/tax.js
@@ -4,8 +4,9 @@ const es = require('elasticsearch')
 const bodybuilder = require('bodybuilder')
 
 class TaxProxy extends AbstractTaxProxy {
-    constructor (config, indexName, taxCountry, taxRegion = ''){
+    constructor (config, entityType, indexName, taxCountry, taxRegion = ''){
         super(config)
+        this._entityType = entityType
         this._indexName = indexName
         this._taxCountry = taxCountry
         this._taxRegion = taxRegion
@@ -28,8 +29,6 @@ class TaxProxy extends AbstractTaxProxy {
                     apiVersion: '5.5',
                     requestTimeout: 5000
                 })
-                
-
                 const esQuery = {
                     index: this._indexName,
                     type: 'taxrule',
@@ -37,7 +36,6 @@ class TaxProxy extends AbstractTaxProxy {
                 }        
                 client.search(esQuery).then(function (taxClasses) { // we're always trying to populate cache - when online
                     inst._taxClasses = taxClasses.hits.hits.map(el => { return el._source })        
-
                     for (let item of productList) {
                         inst.taxFor(item._source, taxClasses, inst._config.tax.defaultCountry, inst._config.tax.defaultRegion)
                     }
