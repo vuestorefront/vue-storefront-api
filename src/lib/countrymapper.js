ec766d3a69dee3ee79ff57eb1817709211ed04ea
pkarw <pkarwatka@divante.pl>
2018-06-04 09:50:59 +0200

    Country mapper fix

--- src/lib/countrymapper.js
+++ src/lib/countrymapper.js
@@ -3,10 +3,12 @@
     let regionId = 0
     for (let country of countryList) {
         if (country.id === countryId) {
-            for (let region of country.available_regions) {
-                if (region.code === regionCode) {
-                    return { regionId: region.id,
-                             regionCode: region.code }
+            if (country.available_regions && country.available_regions.length > 0) {
+                for (let region of country.available_regions) {
+                    if (region.code === regionCode) {
+                        return { regionId: region.id,
+                                regionCode: region.code }
+                    }
                 }
             }
         }
