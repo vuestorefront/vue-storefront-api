6dcaec68192fabc30fd997d1244052ec338a6738
PhoenixDev - YB <yuri.boyko@phoenix-media.eu>
2018-07-18 16:04:40 +0300

    MTH-250 Update schemas with hit sources

--- src/graphql/elasticsearch/product/resolver.js
+++ src/graphql/elasticsearch/product/resolver.js
@@ -1,6 +1,48 @@
 import config from 'config';
 import client from '../client';
 
+const includes = [
+  'type_id',
+  'sku',
+  'name',
+  'tax_class_id',
+  'special_price',
+  'special_to_date',
+  'special_from_date',
+  'price',
+  'priceInclTax',
+  'originalPriceInclTax',
+  'originalPrice',
+  'specialPriceInclTax',
+  'id',
+  'image',
+  'sale',
+  'status',
+  'visibility',
+  'weight',
+  'description',
+  'small_image',
+  'thumbnail',
+  'max_price',
+  'minimal_price',
+  'regular_price',
+  'max_regular_price',
+  'minimal_regular_price',
+  'final_price',
+  'configurable_children.image',
+  'configurable_children.sku',
+  'configurable_children.price',
+  'configurable_children.special_price',
+  'configurable_children.priceInclTax',
+  'configurable_children.specialPriceInclTax',
+  'configurable_children.originalPrice',
+  'configurable_children.originalPriceInclTax',
+  'configurable_children.color',
+  'configurable_children.size',
+  'product_links',
+  'url_key'
+];
+
 async function search(id) {
   let allRecords = '';
   const response = await client.search({
@@ -29,36 +71,31 @@ async function searchList(req) {
     type: 'product',
     body: {
       _source: {
-        includes: [
-          'type_id',
-          'sku',
-          'name',
-          'tax_class_id',
-          'special_price',
-          'special_to_date',
-          'special_from_date',
-          'price',
-          'priceInclTax',
-          'originalPriceInclTax',
-          'originalPrice',
-          'specialPriceInclTax',
-          'id',
-          'image',
-          'sale',
-          'new',
-          'configurable_children.image',
-          'configurable_children.sku',
-          'configurable_children.price',
-          'configurable_children.special_price',
-          'configurable_children.priceInclTax',
-          'configurable_children.specialPriceInclTax',
-          'configurable_children.originalPrice',
-          'configurable_children.originalPriceInclTax',
-          'configurable_children.color',
-          'configurable_children.size',
-          'product_links',
-          'url_key'
-        ]
+        includes: includes
+      },
+      query: {
+        multi_match: {
+          query: req,
+          fields: ['name', 'description']
+        }
+      }
+    }
+  });
+
+  response.hits.hits.forEach(function(hit) {
+    allRecords.push(hit);
+  });
+  return allRecords;
+}
+
+async function searchSourceList(req) {
+  let allRecords = [];
+  const response = await client.search({
+    index: config.elasticsearch.index,
+    type: 'product',
+    body: {
+      _source: {
+        includes: includes
       },
       query: {
         multi_match: {
@@ -79,7 +116,8 @@ async function searchList(req) {
 const resolver = {
   Query: {
     Product: (_, { id }) => search(id),
-    ProductList: (_, { query }) => searchList(query)
+    ProductList: (_, { query }) => searchList(query),
+    ProductSourceList: (_, { query }) => searchSourceList(query)
   }
 };
 
