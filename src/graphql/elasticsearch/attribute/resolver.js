a2ffa7f0034e1dc1e9a995bdb308539db4239b59
PhoenixDev - YB <yuri.boyko@phoenix-media.eu>
2018-08-07 11:13:07 +0300

    MTH-250 Add attributes resolver schema

---
+++ src/graphql/elasticsearch/attribute/resolver.js
@@ -0,0 +1,26 @@
+import config from 'config';
+import client from '../client';
+import { buildQuery } from '../queryBuilder';
+
+async function listAttributes(AttributeInput) {
+
+  console.log(AttributeInput)
+  let query = buildQuery(filter, sort, currentPage, pageSize);
+
+  const response = await client.search({
+    index: config.elasticsearch.indices[0],
+    type: config.elasticsearch.indexTypes[0],
+    body: query
+  });
+
+  return response;
+}
+
+const resolver = {
+  Query: {
+    procustomAttributeMetadataducts: (_, {AttributeInput}) =>
+    listAttributes(AttributeInput)
+  }
+};
+
+export default resolver;
