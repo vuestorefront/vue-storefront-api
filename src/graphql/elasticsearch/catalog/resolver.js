cea5c372dae3bae0ad9aadd39438320063446444
PhoenixDev - YB <yuri.boyko@phoenix-media.eu>
2018-08-08 14:04:07 +0300

    MTH-250 update product response type

--- src/graphql/elasticsearch/catalog/resolver.js
+++ src/graphql/elasticsearch/catalog/resolver.js
@@ -12,6 +12,7 @@ async function list(filter, sort, currentPage, pageSize, search, context, rootVa
     includeFields: config.entities.productListWithChildren.includeFields,
     excludeFields: config.entities.productListWithChildren.excludeFields
   });
+
   const parseURL = context.req.url.replace(/^\/+|\/+$/g, '');
   let urlParts = parseURL.split('/');
   if (urlParts.length < 1) {
@@ -19,11 +20,49 @@ async function list(filter, sort, currentPage, pageSize, search, context, rootVa
   }
   const storeId = parseInt(urlParts[0])
 
-  const response = await client.search({
+  const esResponse = await client.search({
     index: config.elasticsearch.indices[storeId],
     type: config.elasticsearch.indexTypes[0],
     body: query
   });
+
+  let response = {}
+
+  // Process hits
+  response.items = []
+  esResponse.hits.hits.forEach(hit => {
+    let item = hit._source
+    item._score = hit._score
+    response.items.push(item)
+  });
+
+  response.total_count = esResponse.hits.total
+
+  // Convert aggregations to filters
+  // response.filters = prepareFiltersByAggregations(esResponse.aggregations, filter)
+
+  // Process sort
+  let sortOptions = []
+  for (var sortAttribute in sort){
+    sortOptions.push(
+      {
+        label: sortAttribute,
+        value: sortAttribute
+      }
+    )
+  }
+
+
+  response.aggregations = esResponse.aggregations
+  response.sort_fields = {}
+  if (sortOptions.length > 0) {
+    response.sort_fields.options = sortOptions
+  }
+
+  response.page_info = {
+    page_size: pageSize,
+    current_page: currentPage
+  }
 
   return response;
 }
@@ -34,5 +73,34 @@ const resolver = {
       list(filter, sort, currentPage, pageSize, search, context, rootValue)
   }
 };
+
+function prepareFiltersByAggregations(aggregations, filter) {
+  console.log('filter', filter);
+  let filters = {}
+  for (var aggregation in aggregations){
+    for (var attrToFilter in filter){
+      // console.log('incomeFilter', attrToFilter);
+      if (aggregation == 'agg_terms_' + attrToFilter) {
+        const aggregationData = aggregations[aggregation]
+        if (typeof aggregationData !== 'function') {
+          // console.log('aggregation', aggregation);
+          // console.log('aggregationData terms', aggregationData);
+          let aggFilter = {}
+          aggFilter.name = aggregation
+        }
+      }
+      if (aggregation == 'agg_range_' + attrToFilter) {
+        const aggregationData = aggregations[aggregation]
+        if (typeof aggregationData !== 'function') {
+          // console.log('aggregation', aggregation);
+          // console.log('aggregationData range', aggregationData);
+          let aggFilter = {}
+          aggFilter.name = aggregation
+        }
+      }
+    }
+  }
+  return filters
+}
 
 export default resolver;
