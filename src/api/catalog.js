d8b3b287a4949ad56a739db59c30eac8f82438c1
pkarw <pkarwatka@divante.pl>
2018-09-14 09:41:15 +0200

    Minor fix

--- src/api/catalog.js
+++ src/api/catalog.js
@@ -27,7 +27,7 @@ export default ({config, db}) => function (req, res, body) {
 			throw new Error('Invalid / inaccessible index name given in the URL. Please do use following URL format: /api/catalog/<index_name>/_search')
 		}
 
-		if (urlSegments[urlSegments.length - 1] !== '_search') {
+		if (urlSegments[urlSegments.length - 1].indexOf('_search') !== 0) {
 			throw new Error('Please do use following URL format: /api/catalog/<index_name>/_search')
 		}
 	}
