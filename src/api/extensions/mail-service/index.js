8ecc9112670b46a970cdcf2e96eb908e5c1a12d6
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2018-10-11 23:01:55 +0200

    new endpoint for sending email

--- src/api/extensions/mail-service/index.js
+++ src/api/extensions/mail-service/index.js
@@ -1,45 +1,65 @@
 import { apiStatus } from '../../../lib/util';
 import { Router } from 'express';
+import EmailCheck from 'email-check';
 import NodeMailer from 'nodemailer';
 
-module.exports = ({ config, db }) => {
-
+module.exports = ({ config }) => {
   let msApi = Router();
 
   /**
    * POST send an email
    */
   msApi.post('/send-email', (req, res) => {
-    console.log('sdfdsfsd')
-
-    // let userData = req.body
-    // if(!userData.email) {
-    //   apiStatus(res, 'Invalid e-mail provided!', 500)
-    //   return
-    // }
-
-    let transporter = NodeMailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'kz.akbar@gmail.com',
-        pass: 'matahari_1984'
-      }
-    })
-    const mailOptions = {
-      from: userData.sourceAddress,
-      to: userData.targetAddress,
-      subject: userData.subject,
-      text: userData.emailText
+    let userData = req.body
+    if(!userData.sourceAddress) {
+      apiStatus(res, 'Source email address is not provided!', 500)
+      return
     }
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (err) {
-        apiStatus(res, error, 500)
+    if(!userData.targetAddress) {
+      apiStatus(res, 'Target email address is not provided!', 500)
+      return
+    }
+
+    // check if provided email addresses actually exist
+    EmailCheck(userData.sourceAddress)
+    .then(response => {
+      if (response) return EmailCheck(userData.targetAddress)
+      else {
+        apiStatus(res, 'Source email address is invalid!', 500)
         return
       }
-      apiStatus(res, 'OK', 200)
-      transporter.close()
+    })
+    .then(response => {
+      if (response) {
+        let transporter = NodeMailer.createTransport({
+          host: config.extensions.mailService.host,
+          port: config.extensions.mailService.port,
+          secure: config.extensions.mailService.secure,
+          auth: {
+            user: config.extensions.mailService.user,
+            pass: config.extensions.mailService.pass
+          }
+        })
+        const mailOptions = {
+          from: userData.sourceAddress,
+          to: userData.targetAddress,
+          subject: userData.subject,
+          text: userData.emailText
+        }
+        transporter.sendMail(mailOptions, (error) => {
+          if (error) {
+            apiStatus(res, error, 500)
+            return
+          }
+          apiStatus(res, 'OK', 200)
+          transporter.close()
+        })
+      } else {
+        apiStatus(res, 'Target email address is invalid!', 500)
+      }
+    })
+    .catch(() => {
+      apiStatus(res, 'Invalid email address is provided!', 500)
     })
   })
 
