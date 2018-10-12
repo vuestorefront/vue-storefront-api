0a46ff824750edf39305df153be9107a986de33f
Yoann Vié <dboy@dnd.fr>
2018-10-12 15:58:30 +0200

    graphql : allow custom schemas and resolvers addition from extensions level

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,8 +2,12 @@ import path from 'path';
 import config from 'config';
 import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
 
-const typesArray = fileLoader(
+const coreSchemas = fileLoader(
   path.join(__dirname, `./${config.server.searchEngine}/**/*.graphqls`)
 );
+const extensionsSchemas = fileLoader(
+  path.join(__dirname, `../api/extensions/**/*.graphqls`)
+);
+const typesArray = coreSchemas.concat(extensionsSchemas)
 
 export default mergeTypes(typesArray, { all: true });
