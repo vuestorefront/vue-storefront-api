e668fbaf6feb788dffe4068451c67d83338110bb
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2018-10-14 23:34:18 +0200

    added authorization using token for email API

--- src/api/extensions/mail-service/index.js
+++ src/api/extensions/mail-service/index.js
@@ -1,22 +1,42 @@
-import { apiStatus } from '../../../lib/util';
-import { Router } from 'express';
-import EmailCheck from 'email-check';
-import NodeMailer from 'nodemailer';
+import { apiStatus } from '../../../lib/util'
+import { Router } from 'express'
+import config from 'config'
+import EmailCheck from 'email-check'
+import jwt from 'jwt-simple'
+import NodeMailer from 'nodemailer'
 
 module.exports = ({ config }) => {
-  let msApi = Router();
+  const msApi = Router()
+  let token
+
+  /**
+   * GET send token to authorize email
+   */
+  msApi.get('/get-token', (req, res) => {
+    token = jwt.encode(Date.now(), config.extensions.mailService.secretString)
+    apiStatus(res, token, 200)
+  })
 
   /**
    * POST send an email
    */
   msApi.post('/send-email', (req, res) => {
-    let userData = req.body
-    if(!userData.sourceAddress) {
+    const userData = req.body
+    if (!userData.token || userData.token !== token) {
+      apiStatus(res, 'Email is not authorized!', 500)
+    }
+    if (!userData.sourceAddress) {
       apiStatus(res, 'Source email address is not provided!', 500)
       return
     }
-    if(!userData.targetAddress) {
+    if (!userData.targetAddress) {
       apiStatus(res, 'Target email address is not provided!', 500)
+      return
+    }
+    if (!userData.confirmation
+      && !config.extensions.mailService.targetAddressWhitelist.includes(userData.targetAddress)
+    ) {
+      apiStatus(res, 'Target email address is not from the whitelist!', 500)
       return
     }
 
@@ -32,12 +52,12 @@ module.exports = ({ config }) => {
     .then(response => {
       if (response) {
         let transporter = NodeMailer.createTransport({
-          host: config.extensions.mailService.host,
-          port: config.extensions.mailService.port,
-          secure: config.extensions.mailService.secure,
+          host: config.extensions.mailService.transport.host,
+          port: config.extensions.mailService.transport.port,
+          secure: config.extensions.mailService.transport.secure,
           auth: {
-            user: config.extensions.mailService.user,
-            pass: config.extensions.mailService.pass
+            user: config.extensions.mailService.transport.user,
+            pass: config.extensions.mailService.transport.pass
           }
         })
         const mailOptions = {
