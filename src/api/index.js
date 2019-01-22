50b1d9500cf1100275a7ad6dcd343d099acb1700
Bartosz Liburski <bartosz.liburski@gmail.com>
2019-01-22 16:27:08 +0100

    api/index.js tabs fix

--- src/api/index.js
+++ src/api/index.js
@@ -10,56 +10,56 @@ import product from './product';
 import sync from './sync';
 
 export default ({ config, db }) => {
-  let api = Router();
+    let api = Router();
 
-  // mount the catalog resource
-  api.use('/catalog', catalog({ config, db }) )
+    // mount the catalog resource
+    api.use('/catalog', catalog({ config, db }) )
 
-  // mount the order resource
-  api.use('/order', order({ config, db }));
+    // mount the order resource
+    api.use('/order', order({ config, db }));
 
-  // mount the user resource
-  api.use('/user', user({ config, db }));
+    // mount the user resource
+    api.use('/user', user({ config, db }));
 
-  // mount the stock resource
-  api.use('/stock', stock({ config, db }));
+    // mount the stock resource
+    api.use('/stock', stock({ config, db }));
 
-  // mount the review resource
-  api.use('/review', review({ config, db }));
+    // mount the review resource
+    api.use('/review', review({ config, db }));
 
-  // mount the cart resource
-  api.use('/cart', cart({ config, db }));
+    // mount the cart resource
+    api.use('/cart', cart({ config, db }));
 
-  // mount the product resource
-  api.use('/product', product({ config, db }))
+    // mount the product resource
+    api.use('/product', product({ config, db }))
 
-  // mount the sync resource
-  api.use('/sync', sync({ config, db }))
+    // mount the sync resource
+    api.use('/sync', sync({ config, db }))
 
-  // perhaps expose some API metadata at the root
-  api.get('/', (req, res) => {
-    res.json({ version });
-  });
+    // perhaps expose some API metadata at the root
+    api.get('/', (req, res) => {
+      res.json({ version });
+    });
 
-  /** Register the custom extensions */
-  for(let ext of config.registeredExtensions) {
-    let entryPoint
+    /** Register the custom extensions */
+    for(let ext of config.registeredExtensions) {
+      let entryPoint
 
-    try {
-      entryPoint = require('./extensions/' + ext)
-    } catch (err) {
       try {
-        entryPoint = require(ext)
+        entryPoint = require('./extensions/' + ext)
       } catch (err) {
-        console.error(err)
+        try {
+          entryPoint = require(ext)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
+      if (entryPoint) {
+        api.use('/ext/' + ext, entryPoint({ config, db }))
+        console.log('Extension ' + ext + ' registered under /ext/' + ext +' base URL')
       }
     }
 
-    if (entryPoint) {
-      api.use('/ext/' + ext, entryPoint({ config, db }))
-      console.log('Extension ' + ext + ' registered under /ext/' + ext +' base URL')
-    }
-  }
-
-  return api;
+    return api;
 }
