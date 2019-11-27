1e501d0655fd97559b42d2f1a5217d2f58abbe50
cewald <c.ewald@impericon.com>
2019-11-27 18:03:43 +0000

    Add decorator for Storyblok rich-text fields

--- src/api/extensions/icmaa-cms/helpers/formatter/storyblok.js
+++ src/api/extensions/icmaa-cms/helpers/formatter/storyblok.js
@@ -1,5 +1,6 @@
 import pick from 'lodash/pick'
 import config from 'config'
+import StoryblokConnector from '../../connector/storyblok'
 
 const pluginMap = config.extensions.icmaaCms.storyblok.pluginFieldMap
 const metaFieldsToTransport = [{'name': 'uname'}, 'uuid', 'published_at', 'created_at', 'first_published_at']
@@ -9,11 +10,15 @@ const getFieldMap = (key) => pluginMap.find(m => m.key === key)
 export const extractPluginValues = (object) => {
   for (let key in object) {
     let v = object[key]
-    if (typeof v === 'object' && v.plugin) {
-      const map = getFieldMap(v.plugin)
-      if (map) {
-        const values = pick(v, map.values)
-        object[key] = map.values.length === 1 ? Object.values(values)[0] : values
+    if (typeof v === 'object') {
+      if (v.plugin) {
+        const map = getFieldMap(v.plugin)
+        if (map) {
+          const values = pick(v, map.values)
+          object[key] = map.values.length === 1 ? Object.values(values)[0] : values
+        }
+      } else if (v.type === 'doc') {
+        object[key] = StoryblokConnector.api().richTextResolver.render(object[key])
       }
     }
   }
