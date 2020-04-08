60780a23663c5364e859d5d73d9cd5f8207cab3d
cewald <c.ewald@impericon.com>
2020-04-08 13:23:24 +0000

    Make catalog-filter loader less IO-heavy

--- src/helpers/loadCustomFilters.ts
+++ src/helpers/loadCustomFilters.ts
@@ -1,31 +1,26 @@
-import { promises as fs } from 'fs'
 import path from 'path'
-
-function getModuleFilterPaths (moduleName: string, type: string): Promise<any> {
-  const dirPath = path.resolve(__dirname, '../../' + moduleName + '/filter/', type)
-  return fs.readdir(dirPath)
-    .then(files => files.filter(f => f.endsWith('Filter.ts')).map(file => ({ path: path.resolve(dirPath, file), file })))
-    .catch(() => {
-      return []
-    })
-}
 
 export default async function loadModuleCustomFilters (config: Record<string, any>, type: string = 'catalog'): Promise<any> {
   let filters: any = {}
   let filterPromises: Promise<any>[] = []
 
-  for (const ext in config.registeredExtensions) {
-    filterPromises.push(
-      getModuleFilterPaths(config.registeredExtensions[ext], type)
-        .then(filePaths => filePaths.forEach(o => import(o.path)
-          .then(module => {
-            filters[o.file] = module.default
-          })
-          .catch(e => {
-            console.log(e)
-          }))
+  for (const mod of config.registeredExtensions) {
+    if (config.extensions.hasOwnProperty(mod) && config.extensions[mod].hasOwnProperty(type + 'Filter') && Array.isArray(config.extensions[mod][type + 'Filter'])) {
+      const moduleFilter = config.extensions[mod][type + 'Filter']
+      const dirPath = [__dirname, '../api/extensions/' + mod + '/filter/', type]
+      for (const filterName of moduleFilter) {
+        const filePath = path.resolve(...dirPath, filterName + '.ts')
+        filterPromises.push(
+          import(filePath)
+            .then(module => {
+              filters[filterName] = module.default
+            })
+            .catch(e => {
+              console.log(e)
+            })
         )
-    )
+      }
+    }
   }
 
   return Promise.all(filterPromises).then((e) => filters)
