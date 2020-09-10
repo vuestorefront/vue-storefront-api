3e82fb45443ae212cfc162c0688a88b5ca6450ec
Philipp Sander <p.sander@netz98.de>
2020-09-10 11:18:56 +0200

    #404 replace deprecated crypto functions

--- src/lib/test/unit/util.spec.ts
+++ src/lib/test/unit/util.spec.ts
@@ -3,20 +3,20 @@ import * as util from '../../util'
 const SECRET = '__SECRET__'
 
 describe('encryptToken and decryptToken method', () => {
-	describe('decrypt changes parameter', () => {
-		it('Should return something else', () => {
-			const testPassword = 'IAmaPassword123!'
-			const result = util.encryptToken(testPassword, SECRET)
+  describe('decrypt changes parameter', () => {
+    it('Should return something else', () => {
+      const testPassword = 'IAmaPassword123!'
+      const result = util.encryptToken(testPassword, SECRET)
 
-			expect(result).not.toEqual(testPassword);
-		})
-	})
-	describe('decrypt reverses encrypt', () => {
-		it('Should return the password', () => {
-			const testPassword = 'IAmaPassword123!'
-			const result = util.decryptToken(util.encryptToken(testPassword, SECRET), SECRET)
+      expect(result).not.toEqual(testPassword);
+    })
+  })
+  describe('decrypt reverses encrypt', () => {
+    it('Should return the password', () => {
+      const testPassword = 'IAmaPassword123!'
+      const result = util.decryptToken(util.encryptToken(testPassword, SECRET), SECRET)
 
-			expect(result).toEqual(testPassword);
-		})
-	})
+      expect(result).toEqual(testPassword);
+    })
+  })
 })
