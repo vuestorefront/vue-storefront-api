535ee4eb2f09eb0ccfce65e1559ba03f033ecdac
Philipp Sander <p.sander@netz98.de>
2020-09-10 11:07:36 +0200

    #404 replace deprecated crypto functions

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,6 +1,8 @@
 import config from 'config';
 import crypto from 'crypto';
 const algorithm = 'aes-256-ctr';
+const IV_LENGTH = 16;
+const IV = crypto.randomBytes(IV_LENGTH);
 
 /**
  * Get current store code from parameter passed from the vue storefront frotnend app
@@ -96,15 +98,26 @@ export function apiError (res, error) {
   return apiStatus(res, errorMessage, Number(errorCode) || 500);
 }
 
+/**
+ * Create a cypher for the given secret with a max length of 32
+ *
+ * @param secret
+ * @returns {string}
+ */
+function createCypher(secret) {
+  return crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
+}
+
 export function encryptToken (textToken, secret) {
-  const cipher = crypto.createCipher(algorithm, secret)
+  const cipher = crypto.createCipheriv(algorithm, createCypher(secret), IV)
   let crypted = cipher.update(textToken, 'utf8', 'hex')
   crypted += cipher.final('hex');
   return crypted;
 }
 
 export function decryptToken (textToken, secret) {
-  const decipher = crypto.createDecipher(algorithm, secret)
+  let iv = crypto.randomBytes(IV_LENGTH);
+  const decipher = crypto.createDecipheriv(algorithm, createCypher(secret), IV)
   let dec = decipher.update(textToken, 'hex', 'utf8')
   dec += decipher.final('utf8');
   return dec;
